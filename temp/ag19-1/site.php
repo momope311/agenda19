30f98ac903862a7082d957f8be7db63ae658e420
Momano <momope311@gmail.com>
2024-09-28 21:25:09 +0200

    Izmena

---
+++ temp/ag19-1/site.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang='{#html_lang#}'>
+<head>
+	<meta charset='utf-8'>
+	<meta name='description' content='{#meta_desc#}'>
+	<meta name='keywords' content='{#meta_keys#}'>
+	<meta name='author' content='{#meta_auth#}'>
+	<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+	<link rel='icon' href='<?php echo $root."favicon.ico"; ?>'>
+	<title>{#title#}</title>
+	<link rel='stylesheet' href='<?php echo $root."temp/".$temp."/css/styles.css"; ?>'>
+</head>
+<body>
+{#body#}
+</body>
+</html>
