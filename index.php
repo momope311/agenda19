1717a7f58023d2402c8d72cff4918787055612e8
Momano <momope311@gmail.com>
2024-09-28 18:43:42 +0200

    Izmena

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+define('PROTECT','agenda19');
+
+require_once 'config.php';
+require_once 'engine/errors.php';
+require_once 'engine/db.php';
+require_once 'engine/start.php';
+require_once 'engine/content.php';
+require_once 'engine/decode.php';
+
+// Errors
+Errors::Display();
+
+// Set sessions
+Start::Initialize();
+
+// Path
+$path = AppConfig::Path();
+$lett = $path[0];
+$opt1 = $path[1];
+$opt2 = $path[2];
+$opt3 = $path[3];
+$opt4 = $path[4];
+
+// Content output
+$content = new Content();
+
+$decode = new Decode;
+echo $decode->Output();
+
+?>
