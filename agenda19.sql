5974152322ed18e53c2087e714fa2c35bb3b82b3
Momano <momope311@gmail.com>
2024-10-01 14:41:23 +0200

    nova baza

--- agenda19.sql
+++ agenda19.sql
@@ -2,14 +2,20 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 04, 2024 at 11:12 PM
+-- Host: localhost
+-- Generation Time: Oct 01, 2024 at 02:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `agenda19`
@@ -22,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `artid` int(11) NOT NULL AUTO_INCREMENT,
+  `artid` int(11) NOT NULL,
   `header` varchar(128) NOT NULL,
   `seo` varchar(128) NOT NULL,
   `datum` date NOT NULL,
@@ -31,8 +37,7 @@ CREATE TABLE `articles` (
   `pregledi` int(11) NOT NULL,
   `authorid` int(11) NOT NULL,
   `minview` int(11) NOT NULL DEFAULT 400,
-  `comments` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`artid`)
+  `comments` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -42,12 +47,11 @@ CREATE TABLE `articles` (
 --
 
 CREATE TABLE `categories` (
-  `catid` int(11) NOT NULL AUTO_INCREMENT,
+  `catid` int(11) NOT NULL,
   `catname` varchar(64) NOT NULL,
   `catseo` varchar(64) NOT NULL,
   `catdesc` text NOT NULL,
-  `image` varchar(128) NOT NULL,
-  PRIMARY KEY (`catid`)
+  `image` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -57,13 +61,12 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `comments` (
-  `comid` int(11) NOT NULL AUTO_INCREMENT,
+  `comid` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `comment` varchar(256) NOT NULL,
   `artid` int(11) NOT NULL,
   `cdate` datetime NOT NULL,
-  `pub` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`comid`)
+  `pub` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -73,13 +76,71 @@ CREATE TABLE `comments` (
 --
 
 CREATE TABLE `users` (
-  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
   `email` varchar(64) NOT NULL,
   `session` varchar(64) NOT NULL,
-  `usertype` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`userid`)
+  `usertype` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`artid`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`catid`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `artid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `catid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
